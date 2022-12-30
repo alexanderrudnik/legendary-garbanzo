7c4d420b919ef1cc62ac7e9d37b6f59047b36924
leineveber <egor.leineveber@altence.com>
2022-12-31 00:25:28 +0300

    refactor(auth): change bg

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -5,9 +5,13 @@ import BaseContainer from "@/common/components/BaseContainer/BaseContainer";
 import BaseSection from "@/common/components/BaseSection/BaseSection";
 import BaseCenter from "@/common/components/BaseCenter/BaseCenter";
 
-import AuthBG from "@/app/assets/images/auth-bg.webp";
+import AuthBGLight from "@/app/assets/images/auth-bg-light.webp";
+import AuthBGDark from "@/app/assets/images/auth-bg-dark.webp";
+import { useColorModeValue } from "@chakra-ui/react";
 
 const AuthLayout: React.FC = () => {
+  const bg = useColorModeValue(AuthBGLight, AuthBGDark);
+
   return (
     <main>
       <BaseSection
@@ -16,7 +20,7 @@ const AuthLayout: React.FC = () => {
         display="flex"
         justifyContent="center"
         alignItems="center"
-        backgroundImage={AuthBG}
+        backgroundImage={bg}
         backgroundSize="cover"
       >
         <BaseCenter width="100%" maxWidth={400}>
