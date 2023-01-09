ffb892ea1cd679e03390c66008e298b59bd33fcd
leineveber <egor.leineveber@altence.com>
2023-01-09 15:19:32 +0300

    feat(my-workspace): tabs

--- src/features/workspace/pages/MyWorkspace.tsx
+++ src/features/workspace/pages/MyWorkspace.tsx
@@ -1,7 +1,21 @@
+import BaseSection from "@/common/components/BaseSection/BaseSection";
+import BaseTabs from "@/common/components/BaseTabs/BaseTabs";
 import React from "react";
 
 const MyWorkspace: React.FC = () => {
-  return <div>MyWorkspace</div>;
+  return (
+    <BaseSection>
+      <BaseTabs
+        variant="enclosed-colored"
+        isFitted
+        tabs={[
+          { label: "User settings", panel: "Soon" },
+          { label: "Workspace settings", panel: "Soon" },
+          { label: "Notification settings", panel: "Soon" },
+        ]}
+      />
+    </BaseSection>
+  );
 };
 
 export default MyWorkspace;
