d6c56a86c65935446203410a8afa94aef3bfb82e
leineveber <egor.leineveber@altence.com>
2023-02-28 13:24:49 +0300

    fix(basetextarea): fix border color

--- src/common/components/BaseTextArea/BaseTextArea.tsx
+++ src/common/components/BaseTextArea/BaseTextArea.tsx
@@ -1,3 +1,24 @@
-import { Textarea as BaseTextArea } from "@chakra-ui/react";
+import React from "react";
+import {
+  Textarea,
+  TextareaProps,
+  useColorModeValue,
+  useTheme,
+} from "@chakra-ui/react";
+
+export interface BaseTextAreaProps extends TextareaProps {}
+
+const BaseTextArea = React.forwardRef<any, BaseTextAreaProps>(
+  ({ ...props }, ref) => {
+    const theme = useTheme();
+
+    const color = useColorModeValue(
+      theme.colors.primary[500],
+      theme.colors.primary[200]
+    );
+
+    return <Textarea ref={ref} focusBorderColor={color} {...props} />;
+  }
+);
 
 export default BaseTextArea;
