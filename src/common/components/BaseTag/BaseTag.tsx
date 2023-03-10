620e18fa7c0abcbea629b572cb65e88076b2b8c6
leineveber <egor.leineveber@altence.com>
2023-03-10 16:33:14 +0300

    refactor(basetag): change colors

--- src/common/components/BaseTag/BaseTag.tsx
+++ src/common/components/BaseTag/BaseTag.tsx
@@ -1,4 +1,10 @@
-import { Tag, TagCloseButton, TagLabel, TagProps } from "@chakra-ui/react";
+import {
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  TagProps,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 
 interface Props extends TagProps {
@@ -12,8 +18,17 @@ const BaseTag: React.FC<Props> = ({
   children,
   ...props
 }) => {
+  const color = useColorModeValue(
+    "var(--chakra-colors-primary-700)",
+    "var(--chakra-colors-primary-200)"
+  );
+  const bg = useColorModeValue(
+    "var(--chakra-colors-primary-50)",
+    "rgba(162, 216, 214, 0.16)"
+  );
+
   return (
-    <Tag {...props}>
+    <Tag color={color} background={bg} {...props}>
       <TagLabel>{children}</TagLabel>
       {close && <TagCloseButton onClick={onClose} />}
     </Tag>
