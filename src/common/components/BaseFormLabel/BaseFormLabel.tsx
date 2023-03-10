9d4e4b3258fff94260264b1dec3cb6192af76b71
leineveber <egor.leineveber@altence.com>
2023-03-10 20:00:55 +0300

    fix(base-form-label): inline-block

--- src/common/components/BaseFormLabel/BaseFormLabel.tsx
+++ src/common/components/BaseFormLabel/BaseFormLabel.tsx
@@ -1,3 +1,10 @@
-import { FormLabel as BaseFormLabel } from "@chakra-ui/react";
+import React from "react";
+import { FormLabel, FormLabelProps } from "@chakra-ui/react";
+
+export interface BaseFormLabelProps extends FormLabelProps {}
+
+const BaseFormLabel: React.FC<BaseFormLabelProps> = ({ ...props }) => {
+  return <FormLabel display="inline-block" {...props} />;
+};
 
 export default BaseFormLabel;
